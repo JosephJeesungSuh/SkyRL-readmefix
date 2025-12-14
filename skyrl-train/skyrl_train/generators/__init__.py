e816624f97b925896768e2ef668876a4467d0086
Joseph Suh <jjssuh@rnn.ist.berkeley.edu>
2025-12-14 02:25:41 -0800

    adding __init__ in skyrl train generators

--- skyrl-train/skyrl_train/generators/__init__.py
+++ skyrl-train/skyrl_train/generators/__init__.py
@@ -1,4 +1,5 @@
 from .base import GeneratorInterface, GeneratorInput, GeneratorOutput
 from .skyrl_gym_generator import SkyRLGymGenerator
+from .user_simulator import UserSimulator
 
-__all__ = ["GeneratorInterface", "GeneratorInput", "GeneratorOutput", "SkyRLGymGenerator"]
+__all__ = ["GeneratorInterface", "GeneratorInput", "GeneratorOutput", "SkyRLGymGenerator", "UserSimulator"]
